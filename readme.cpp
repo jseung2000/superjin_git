5e0c5b4a4a7d4fee2067151d7bc13a25d60e8924
jseung <jin.seung@ericsson.com>
2015-10-29 11:37:02 +0900

    readme.cpp Add

---
+++ readme.cpp
@@ -0,0 +1,25 @@
+/*
+ * readme.cpp
+ *
+ *  Created on: 2015. 10. 29.
+ *      Author: ejinseu
+ */
+
+//============================================================================
+// Name        : Hello.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Read me!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
+
+
