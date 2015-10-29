4985fdd207d31eadfc013f59dede88be6b05ee99
jseung <jin.seung@ericsson.com>
2015-10-29 11:22:31 +0900

    Hello Seung Jin Add

--- Hello Jin.cpp
+++ Hello Jin.cpp
@@ -13,5 +13,6 @@ int main() {
 	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
 	cout << "!!!Hello Jin!!!" << endl; // prints !!!Hello World!!!
 	cout << "!!!Hello Seung!!!" << endl; // prints !!!Hello World!!!
+	cout << "!!!Hello Seung Jin!!!" << endl; // prints !!!Hello World!!!
 	return 0;
 }
